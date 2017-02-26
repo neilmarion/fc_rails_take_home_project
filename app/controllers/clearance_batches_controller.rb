b554154a996d4d40f4360e4bc4fcf22f62eae13d
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-02-26 19:10:38 +0800

    Implement the reports

--- app/controllers/clearance_batches_controller.rb
+++ app/controllers/clearance_batches_controller.rb
@@ -7,6 +7,7 @@ class ClearanceBatchesController < ApplicationController
   def show
     @clearance_batch  = ClearanceBatch.find(params[:id])
     @items = Item.where(clearance_batch_id: params[:id])
+    generate_report
   end
 
   def create
@@ -24,6 +25,41 @@ class ClearanceBatchesController < ApplicationController
     end
     flash[:alert] = alert_messages.join("<br/>") if alert_messages.any?
     redirect_to action: :index
+  end
+
+  private
+
+  def generate_report
+    items_hashes = @items.map do |item|
+      {style: item.style.type, size: item.size, color: item.color}
+    end
+
+    report = []
+
+    items_hashes.each do |items_hash|
+      specific_count = {
+        description: "#{items_hash[:size]} #{items_hash[:color]} #{items_hash[:style]}", count: 0
+      }
+
+      items_hashes.each do |i|
+        if items_hash == i
+          specific_count[:count] = specific_count[:count] + 1
+        end
+      end
+
+      report << specific_count
+    end
+
+    @report_texts = []
+    report.uniq.each do |row|
+      @report_texts << "#{row[:count]} pcs of #{row[:description]}"
+    end
+
+    @total_price = compute_total_price
+  end
+
+  def compute_total_price
+    @items.sum(:price_sold).to_f
   end
 
 end
